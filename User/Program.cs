fa50ba5d14c648242db93917c4a13f33a27abaa4
seyidovmurad <seyidovmurad@outlook.com>
2022-09-29 16:59:01 +0400

    Updated db and Added Crud

--- User/Program.cs
+++ User/Program.cs
@@ -1,7 +1,12 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using User.Data;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+var conn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=WorkDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+builder.Services.AddDbContext<WorkDbContext>(option => option.UseSqlServer(conn));
 
 var app = builder.Build();
 
@@ -12,6 +17,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
